5da4a3e2f240ca811b8c5d92874399aff12688d3
Saaim0 <saaimbabar@hotmail.com>
2025-11-20 16:14:42 -0500

    Update sketchpad_sketch.js

--- inverse_ai/sketchpad_sketch.js
+++ inverse_ai/sketchpad_sketch.js
@@ -4,8 +4,19 @@ var buttonBGChange;
 var sliderSize;
 var checkType;
 
+// Saaim addition variables
+let drawArea;          // Drawing area
+let showDraw = false;  // Variable to activate second canvas
+let drawDimX = 720;
+let drawDimY = 400;
+let btnDraw;
+
+let promptArray= [
+  
+]
+
 function setup(){
-    createCanvas(720,400);
+    createCanvas(1020,400);
     createDiv("Create. Whatever. Shit. You want.");
 
     pallet = createColorPicker("black");
@@ -16,6 +27,20 @@ function setup(){
     buttonBGChange = createButton("Change background color");
     buttonBGChange.mousePressed(BGChange);
     checkType = createCheckbox("Seamless brush")
+  
+  // Saaim drawing canvas setup
+  btnDraw = createButton("Click me");
+  btnDraw.position(width - 70, height + 10);
+  btnDraw.mousePressed(() => {
+    // Button to activate drawing area
+    if (!drawArea) {
+      drawArea = createGraphics(300, 400); // Drawing area size
+      drawArea.background(220);
+    }
+    showDraw = true;
+  });
+  
+  promptDisplay();
 }
 
 function draw(){
@@ -31,8 +56,29 @@ function draw(){
         }
         
     }
+  
+  // Saaim drawing canvas
+  // Makes the drawing area visible
+  if (showDraw && drawArea) {
+    if ( // Checks mouse position to see if it is on the canvas
+      mouseIsPressed && 
+      mouseX >= drawDimX && mouseX <= width &&
+      mouseY >= 0 && mouseY <= height) {
+      
+      // Draws lines when mouseIsPressed
+      drawArea.stroke(0);
+      drawArea.strokeWeight(3);
+      drawArea.line(pmouseX - drawDimX, pmouseY - 0, mouseX - drawDimX,  mouseY - 0); // Replace 0 with drawDimY if moving position of drawing area
+    }
+    // Draw the drawArea onto the main canvas on the right
+    image(drawArea, drawDimX, 0); // Replace 0 with drawDimY if moving position of drawing area
+  } 
+  
 }
 
+function promptDisplay() {
+  
+}
 function BGChange(){
     background(paper.color());
 }
