3b7b6d817dbc7bfe86478b5c7fa7d3d1533fee96
manuelquiambao <manueltquiambao@gmail.com>
2025-11-26 01:59:50 -0500

    new minor functions added
    - brush size slider functionality now working!
    - added "erase all" function
    - drawable image + prompt shows before
    - added a message array for future use
    
    notes/things to consider:
    - resizing during runtime still makes drawable image buggy
    - harken to how AI generates images in future i.e. reverse diffusion?

--- inverse_ai/sketchpad_sketch.js
+++ inverse_ai/sketchpad_sketch.js
@@ -2,100 +2,139 @@ var pallet;
 var paper;
 var buttonBGChange;
 var sliderSize;
-var checkType;
+// var checkType;
 
 // Saaim addition variables
 let drawArea;          // Drawing area
 let showDraw = false;  // Variable to activate second canvas
-let drawDimX = 720;
-let drawDimY = 400;
+let drawDimX;
+let drawDimY;
 let btnDraw;
 
+// Manuel vars
+let canvas;
+let canvasW;
+let canvasH;
+let drawW;
+let drawH;
+let generatedImages = [];
+let messages = [];
+let btnReset;
+
 let promptArray= [
-  
-]
+  "Generate an image of a curious cat",
+  "Generate an image of a futuristic city of hope",
+  "Generate a portrait of an artist",
+  "Generate an image of a 1980's Diner",
+  "Generate an image of a lonely flower",
+  "Generate an image of happiness",
+  "Generate an image of what you see right now",
+  "Generate an image of the future",
+  "Generate an image of the cycle of life",
+  "Generate an image of something you have just seen"
+];
 
+let displayText = "";
+
+function adjustCanvas() {
+  canvasW = windowWidth; canvasH = windowHeight;
+  drawW = canvasW/2; drawH = canvasH;
+}
+
+function eraseCanvas() {
+  drawArea.background(220);
+}
+
+function windowResized() {
+  adjustCanvas();
+}
 function setup(){
-    createCanvas(1020,400);
-    createDiv("Create. Whatever. Shit. You want.");
-
+  adjustCanvas();
+  canvas = createCanvas(canvasW, canvasH);
+  
+    //createCanvas(windowWidth,windowHeight-40);
+  
+    drawDimX = drawW;
+    drawDimY = drawH;
+    
+    // drawing canvas function moved up since it will be visible at all times
+    drawArea = createGraphics(drawDimX, drawDimY); // Drawing area size
+    drawArea.background(220);
+    showDraw = true;
+    
     pallet = createColorPicker("black");
     paper = createColorPicker("white");
-    sliderSize = createSlider(1, 50, 10, 1)
-
-    background(paper.color());
+    sliderSize = createSlider(1, 50, 10, 1); // for brush size
+  
     buttonBGChange = createButton("Change background color");
     buttonBGChange.mousePressed(BGChange);
-    checkType = createCheckbox("Seamless brush")
+    // checkType = createCheckbox("Seamless brush")
   
   // Saaim drawing canvas setup
   btnDraw = createButton("Click me");
   btnDraw.position(width - 70, height + 10);
   btnDraw.mousePressed(() => {
-    // Button to activate drawing area
     if (!drawArea) {
-      drawArea = createGraphics(300, 400); // Drawing area size
+    }
+    if (drawArea) {
       drawArea.background(220);
     }
-    showDraw = true;
+    promptDisplay();
   });
   
+  //reset canvas
+  btnReset = createButton("Reset");
+  btnReset.mousePressed(() => {
+    if (drawArea) {
+      drawArea.background(220);
+    }
+  })
   promptDisplay();
 }
 
 function draw(){
-    if(mouseIsPressed === true){
-        if (checkType.checked()) {
-            stroke(pallet.color());
-            strokeWeight(sliderSize.value());
-            line(pmouseX,pmouseY,mouseX,mouseY);
-        }  else {
-            fill(pallet.color());
-            strokeWeight(0);
-            ellipse(mouseX,mouseY,sliderSize.value(),sliderSize.value());
-        }
-        
-    }
+  background(255);
   
   // Saaim drawing canvas
   // Makes the drawing area visible
   if (showDraw && drawArea) {
-    if ( // Checks mouse position to see if it is on the canvas
-      mouseIsPressed && 
-      mouseX >= drawDimX && mouseX <= width &&
-      mouseY >= 0 && mouseY <= height) {
-      
+    if (
+      mouseIsPressed && //if mouse is clicked or held...
+      mouseX >= drawDimX && mouseX <= width && // ...is within bounds of the drawArea's width...
+      mouseY >= 0 && mouseY <= height) // ...and is within bounds of the drawArea's height...
+      { 
       // Draws lines when mouseIsPressed
       drawArea.stroke(0);
-      drawArea.strokeWeight(3);
+      drawArea.strokeWeight(sliderSize.value());
       drawArea.line(pmouseX - drawDimX, pmouseY - 0, mouseX - drawDimX,  mouseY - 0); // Replace 0 with drawDimY if moving position of drawing area
     }
     // Draw the drawArea onto the main canvas on the right
-    image(drawArea, drawDimX, 0); // Replace 0 with drawDimY if moving position of drawing area
+    image(drawArea, windowWidth/2, 0); // Replace 0 with drawDimY if moving position of drawing area
   } 
   
+  push();
+        textSize(32);
+        rectMode(CENTER);
+        text(messages[messages.length-1],(width/2)/2,height/2,((width/2)/2)-(width/8),height/2);
+        pop();
 }
 
 function promptDisplay() {
+  // gets number of prompt from a list
+  let rand = int(random(0,promptArray.length-1));
+  
+  // pushes prompt to the array
+  messages.push(promptArray[rand]);
+}
+
+function computerPromptsYou() {
   
 }
+
 function BGChange(){
     background(paper.color());
 }
 
-function keyPressed(){
-    if (keyCode === 32) {
-        background(paper.color());
-    }
-}
-
-// function draw(){
-    //if(mouseIsPressed === true){
-        //fill(0,0,0);
-        //noStroke();
-        //ellipse(mouseX,mouseY,5,5);
-    //}
-    //else{
-        //background(200,200,200,8);
-    //}
-//}
+//logDiv = createDiv() for displaying chat history
+//logDiv.style('overflow-y','auto') for scrollability;
+//logs will be an array showing objects
